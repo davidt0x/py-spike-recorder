d083004c601e02cae302335cf85d6f48ee43ec78
David Turner <davidt0x@gmail.com>
2021-01-18 15:02:19 -0500

    Basic Iowa experiment is complete.

--- src/spike_recorder/experiments/iowa/app.py
+++ src/spike_recorder/experiments/iowa/app.py
@@ -6,6 +6,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from spike_recorder.experiments.iowa.instructions_ui import Ui_dialog_instructions
 from spike_recorder.experiments.iowa.iowa_ui import Ui_main_window
+from spike_recorder.experiments.iowa.win_message_ui import Ui_Dialog as Ui_win_message
+from spike_recorder.experiments.iowa.deck import Deck
+from spike_recorder.experiments.iowa.data import IowaData
 
 # It seems I need to add this to get trace backs to show up on
 # uncaught python exceptions.
@@ -55,6 +58,39 @@ class IntroDialog(QtWidgets.QDialog, Ui_dialog_instructions):
         self.textbox_file.setText(foo_dir)
 
 
+class WinDialog(QtWidgets.QDialog, Ui_win_message):
+    """
+    A modal dialog that displays wins and losses from a deck pull and pauses the
+    experiement for a bit.
+    """
+
+    def __init__(self, parent=None, win_amount=0, loss_amount=0, delay_seconds=3):
+        QtWidgets.QDialog.__init__(self, parent)
+        self.setupUi(self)
+
+        self.label.setText(f'<html><head/><body><p style="color:#0571b0">Win: ${win_amount}</p>'
+                           f'<p style="color:#ca0020">Loss: ${loss_amount}</p></body></html>')
+
+        self.lcdNumber.display(delay_seconds)
+
+        # Setup a time to invoke the render function and see if its time to close
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update)
+        timer.start(1000)
+
+    def update(self):
+        """
+        Update the countdown timer, close the dialog when it is done.
+
+        Returns:
+            None
+        """
+        self.lcdNumber.display(self.lcdNumber.intValue() - 1)
+
+        if self.lcdNumber.intValue() == 0:
+            self.done(0)
+
+
 class IowaMainWindow(QtWidgets.QMainWindow, Ui_main_window):
     """
     The main window GUI for the Iowa Gambling task experiment.
@@ -70,6 +106,9 @@ class IowaMainWindow(QtWidgets.QMainWindow, Ui_main_window):
     # The starting winnings
     INITIAL_WINNINGS = 2000
 
+    # How long to pause between deck pulls
+    DELAY_SECS = 3
+
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
@@ -80,12 +119,114 @@ class IowaMainWindow(QtWidgets.QMainWindow, Ui_main_window):
         self.last_win = 0
         self.last_loss = 0
         self.deck_pull_index = 0
+        self.is_hunch = False
+        self.is_sure = False
 
         self.progress_winnings.setMaximum(self.MAX_WINNINGS)
         self.progress_losses.setMaximum(self.MAX_LOSSES)
 
+        # Define the deck behaviour
+        deck1 = Deck.make_deck(win_amounts=100,
+                               loss_amounts=[0, 150, 200, 250, 300, 350],
+                               loss_weights=[50, 10, 10, 10, 10, 10])
+        deck2 = Deck.make_deck(win_amounts=100,
+                               loss_amounts=[0, 1250],
+                               loss_weights=[90, 10])
+        deck3 = Deck.make_deck(win_amounts=50,
+                               loss_amounts=[0, 25, 50, 75],
+                               loss_weights=[40, 30, 20, 10])
+        deck4 = Deck.make_deck(win_amounts=50,
+                               loss_amounts=[0, 250],
+                               loss_weights=[90, 10])
+
+        # Assign each deck to a button
+        self.decks = {self.deck_button1: deck1,
+                      self.deck_button2: deck2,
+                      self.deck_button3: deck3,
+                      self.deck_button4: deck4}
+
+        # Send all the deck button presses to a single handler
+        for button, deck in self.decks.items():
+            button.clicked.connect(self.deck_button_pressed)
+
+        self.hunch_button.clicked.connect(self.update_hunch)
+
         # Update the status
         self.update_status()
+
+        # Setup the data recording
+        self.data = IowaData()
+
+    def deck_button_pressed(self):
+        """
+        The user has drawn from a deck. Lets see what they win. This is the main event of the application.
+
+        Returns:
+            None
+        """
+
+        # Get the button that was clicked
+        button = self.sender()
+
+        # Get the deck for this button
+        deck = self.decks[button]
+
+        deck_index = list(self.decks.values()).index(deck)
+
+        # Pull a card from this deck
+        (win_amount, loss_amount) = deck.pull()
+
+        self.winnings = self.winnings + win_amount
+        self.losses = self.losses + loss_amount
+        self.last_win = win_amount
+        self.last_loss = loss_amount
+
+        # Update the status display
+        self.update_status()
+
+        # Add the data and rewrite the experiemental data to the file.
+        self.data.add_trial(deck=deck_index, win_amount=win_amount, loss_amount=loss_amount,
+                            hunch=self.is_hunch, sure=self.is_sure)
+
+        # Dump the data back out to file. We will just dump everything back out over and over again
+        # so that if the user stops half way through, they have part of their data.
+        if self.output_filename is not None:
+            self.data.to_csv(self.output_filename)
+        else:
+            logging.warning("Output filename is not definied, results are not being saved.")
+
+        if self.DELAY_SECS and self.DELAY_SECS > 0:
+            win_diag = WinDialog(win_amount=win_amount, loss_amount=loss_amount, delay_seconds=self.DELAY_SECS)
+            win_diag.exec_()
+
+    def update_hunch(self):
+        """
+        Allow the user to record when they get a hunch by pressing a button.
+
+        Returns:
+            None
+        """
+        if not self.is_hunch:
+            self.is_hunch = True
+            self.hunch_button.setText("I am pretty sure!")
+
+            # Set event to spike recorder to mark in the recording when a hunch was recorded
+        else:
+            if not self.is_sure:
+                self.is_sure = True
+                self.hunch_button.setEnabled(False)
+
+                # Set and event in the spike recorder to mark in the recording when the user is sure.
+
+
+    def get_num_pulls(self):
+        """
+        Check how many pulls we have made on the decks.
+
+        Returns:
+            The number of pulls
+        """
+        return sum([deck.num_pulls for button, deck in self.decks.items()])
 
     def update_status(self):
         """
@@ -99,6 +240,7 @@ class IowaMainWindow(QtWidgets.QMainWindow, Ui_main_window):
         net = self.last_win - self.last_loss
         color = "#0571b0" if net >= 0 else "#ca0020"
         self.label_net_win.setText(f'Net Winnings: <font color="{color}">${net}</font>')
+        self.label_pull_count.setText(f"Pull {self.get_num_pulls()}/{self.MAX_DECK_PULLS}")
 
         # Update the progress bars
         self.progress_winnings.setValue(self.winnings)
