ae6a825e96c521fe701b46a720fddf1e549bdb67
David Turner <davidt0x@gmail.com>
2021-01-24 17:00:12 -0500

    Fix MacOS multiprocessing crash

--- src/spike_recorder/server/__init__.py
+++ src/spike_recorder/server/__init__.py
@@ -1,17 +1,41 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import platform
+import multiprocessing
+
 from multiprocessing import Process, Queue
 
 from ._core import __doc__, run
 
-def run_wrapper():
+
+# Fix for MacOS High Sierra, see:
+# https://stackoverflow.com/questions/30669659/multiproccesing-and-error-the-process-has-forked-and-you-cannot-use-this-corefou
+if platform.system() == "Darwin":
+    multiprocessing.set_start_method('spawn')
+
+
+def _run_wrapper():
+    """
+    A simple wrapper that changes directory before launching the SpikeRecorder pybind11 module. The module
+    needs to run from its location directory because it looks for files.
+    """
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     run()
 
 
-def launch(is_async: bool = False):
-    p = Process(target=run_wrapper)
+def launch(is_async: bool = False) -> multiprocessing.Process:
+    """
+    Lauch the Backyard Brains Spike recorder application. This function launches a subprocess.
+
+    Args:
+        is_async: Should the this function run asynchronously. That is, should it return instantly or
+            block until the application closes.
+
+    Returns:
+        The Process containing the SpikeRecorder application.
+    """
+    p = Process(target=_run_wrapper)
     p.start()
 
     if not is_async:
